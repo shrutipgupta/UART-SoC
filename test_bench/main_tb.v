f1118a24897ef7c16794de980e78aea5ad72fd72
Shruti Prakash <45819097+shrutipgupta@users.noreply.github.com>
2020-04-17 19:21:47 +0530

    Update main_tb.v

--- test_bench/main_tb.v
+++ test_bench/main_tb.v
@@ -22,7 +22,7 @@
 
 module main_tb();
 
-reg clk, ideal, intt, ideal_rx, rx;
+reg clk, rst, ideal, intt, ideal_rx, rx;
 reg [7:0]lcr, dll, dlh;
 wire tx;
 
@@ -30,26 +30,46 @@ always #5 clk = ~clk;
 
 initial 
 begin 
-    //The reciever clock is derived from the main clock by dividing with 16. Thus an ideal pulse is given for 50 ns and then the rx bits are 
-    //given as a pulse for 300 ns. (i.e. any value of rx is maintained for 300 ns)
-    clk <= 0; ideal_rx <= 1'b1; ideal <= 1'b1; intt <= 1'b0; lcr <= 8'b1000_1011; dll <= 8'b0000_0010; dlh <= 8'b0000_0000; rx <= 1'b0;#50;
-    //Initially no transmission or reception (No interrupt issues as nothing is transmitted)
+    //clock is started with the rst signal being drawn low from a high value
+    //Initially no transmission or reception occurs(No interrupt issues as nothing is transmitted) - unless ideal and ideal_rx are drawn from high to low
     //For parity style (xx0 No parity bit, 001 Odd, 011 Even, 101 High stick parity, 111 Low stick parity
+    //The transmission and reception occur at baud rate - it is determined by dividing the original clock with the divisor followed by division with 16 as receiver samples 16 times in one clock cycle. 
     //Tx is generated at negedge
     //Rx is sampled at negedge
-    ideal_rx <= 1'b0; ideal <= 1'b0; rx <= 1'b1;#300;
-    rx <= 1'b0;#300;
-    rx <= 1'b0;#300;
-    rx <= 1'b1;#300;
-    rx <= 1'b0; intt <= 1'b1;#50;
-    intt <= 1'b0;#300;
-    rx <= 1'b1;#300;
-    rx <= 1'b0;#300;
-    rx <= 1'b1;#300;
-    rx <= 1'b1;#300;
-    rx <= 1'b0;#300;
-    rx <= 1'b0;#300;
+        
+        clk<=0; rst<=1; ideal<=1; ideal_rx<=1; intt<=0; lcr<=8'b1000_1011; dll<=8'b0000_0010; dlh<=8'b0000_0000; rx<=0;#20;
+        rst<=0; ideal<=0; ideal_rx<=0;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b1;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b0;#1200;
+        rx <= 1'b0;#1200;
+        #9000;
+        intt<=1;#4000;
+        intt<=0;
 end
 
-interface uut(.clk(clk), .ideal_rx(ideal_rx), .ideal(ideal), .intt(intt), .lcr(lcr), .dll(dll), .dlh(dlh), .rx(rx), .tx(tx));  
+//The final design does not provide LCR, DLL and DLH as input, however, for running the simulations they are kept as output, thus the commented function instantiation should be used for simulation.
+    //The function heading would also need to be changed (use the commented one).
+// interface uut(.clk(clk), .rst(rst), .ideal(ideal), .intt(intt), .lcr(lcr), .dll(dll), .dlh(dlh), .ideal_rx(ideal_rx), .rx(rx), .tx(tx));  
+interface uut(.clk(clk), .rst(rst), .ideal(ideal), .intt(intt), .ideal_rx(ideal_rx), .rx(rx), .tx(tx));  
 endmodule
