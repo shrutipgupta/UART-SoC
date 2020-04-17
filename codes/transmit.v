4d3278b682b7346ea4b646e4e1b131f3157b6206
Shruti Prakash <45819097+shrutipgupta@users.noreply.github.com>
2020-04-17 19:15:25 +0530

    Update transmit.v

--- codes/transmit.v
+++ codes/transmit.v
@@ -28,52 +28,47 @@ input [1:0]parity;
 input [7:0]thr, prev;
 output reg [7:0]thr_out;
 output reg tx;
-reg mode;
+reg [1:0]mode;
 
 always @(posedge br)
 begin
     if({start,stop}==2'b10)
-    begin
-        tx = 1'b0;
-        thr_out = thr;
-        mode = 1'b1;
-    end
+        mode = 2'b00;
     else 
         if({start,stop}==2'b01)
-        begin
-            tx = 1'b1;
-            mode = 1'b1;
-        end
+            mode = 2'b01;
         else
             if(parity[0]==1'b1)
-            begin
-                tx = parity[1];
-                mode = 1'b1;
-            end
+                mode = 2'b10;
             else
-            begin
-                mode = 1'b0;
-                if(intt == 1'b1)
-                    thr_out = prev;
-                else
-                    thr_out = thr;
-            end
-        
+                mode = 2'b11;     
 end
 
 always @(negedge br)
 begin
-    if(mode == 1'b0)
-    begin
-    thr_out[7] <= 1'b0;
-    thr_out[6] <= thr_out[7];
-    thr_out[5] <= thr_out[6];
-    thr_out[4] <= thr_out[5];
-    thr_out[3] <= thr_out[4];
-    thr_out[2] <= thr_out[3];
-    thr_out[1] <= thr_out[2];
-    thr_out[0] <= thr_out[1];
-    tx <= thr_out[0];
-    end
-end             
+    case(mode[1:0])
+        3'b00: 
+        begin 
+            tx <= 1'b0;
+            if(intt == 1'b1)
+                thr_out <= prev;
+            else
+                thr_out <= thr;
+        end
+        3'b01: tx <= 1'b1;
+        3'b10: tx <= parity[1];
+        3'b11:
+        begin
+            thr_out[7] <= 1'b0;
+            thr_out[6] <= thr_out[7];
+            thr_out[5] <= thr_out[6];
+            thr_out[4] <= thr_out[5];
+            thr_out[3] <= thr_out[4];
+            thr_out[2] <= thr_out[3];
+            thr_out[1] <= thr_out[2];
+            thr_out[0] <= thr_out[1];
+            tx <= thr_out[0];
+        end
+    endcase
+end        
 endmodule
