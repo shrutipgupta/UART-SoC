edbdfa591820668adf5f0b65ea166e594f2fa400
Shruti Prakash <45819097+shrutipgupta@users.noreply.github.com>
2020-04-17 19:18:58 +0530

    Update clk_div_tb.v

--- test_bench/clk_div_tb.v
+++ test_bench/clk_div_tb.v
@@ -21,16 +21,16 @@
 
 
 module clk_div_tb();
-reg clk;
+reg clk,rst;
 reg [7:0]dll,dlh;
 wire br;
-wire [15:0]counter;
 
 always #5 clk = ~clk;
 initial 
 begin
-clk <= 1; dll <= 8'b0000_0110; dlh <= 8'b0000_0000;
+clk <= 1; rst=1; dll <= 8'b0000_0110; dlh <= 8'b0000_0000;#10;
+rst = 0;
 end
 
-baud_gen uut(.clk(clk), .br(br), .dll(dll), .dlh(dlh[3:0]), .counter(counter));
+baud_gen uut(.clk(clk), .rst(rst), .br(br), .div({dlh,dll}));
 endmodule
